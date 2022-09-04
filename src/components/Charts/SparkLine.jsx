ec5dbe8be958f1c2602e5855092164739bc35d09
elmergustavo <elmergustavo79@gmail.com>
2022-09-04 11:41:54 -0600

    update data

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-class SparkLine extends React.PureComponent {
-  render() {
-    const { id, height, width, color, data, type, currentColor } = this.props;
+const SparkLine = ({id, height, width, color, data, type, currentColor}) => {
 
     return (
       <SparklineComponent
@@ -16,7 +14,6 @@ class SparkLine extends React.PureComponent {
         border={{ color: currentColor, width: 2 }}
         tooltipSettings={{
           visible: true,
-          // eslint-disable-next-line no-template-curly-in-string
           format: '${x} : data ${yval}',
           trackLineSettings: {
             visible: true,
@@ -31,7 +28,7 @@ class SparkLine extends React.PureComponent {
         <Inject services={[SparklineTooltip]} />
       </SparklineComponent>
     );
-  }
+  
 }
 
 export default SparkLine;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -13,4 +13,3 @@ export { default as Stacked } from './Charts/Stacked';
 export { default as Pie } from './Charts/Pie';
 export { default as ChartsHeader } from './ChartsHeader';
 export { default as Header } from './Header';
-
